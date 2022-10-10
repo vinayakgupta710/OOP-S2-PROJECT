d75dae9eee34a93837c2f417a9e6ffabc7242db5
Vinayak Gupta <guptavinayak710@gmail.com>
2022-10-10 21:47:08 +1030

    added remove movie from favlist func

--- User_preferences.cpp
+++ User_preferences.cpp
@@ -202,3 +202,153 @@ void User_preferences::printFavMovies(){
 
     delete[] favMovieArr;
 }
+
+void User_preferences::removeMovieFromFavourites(std::string title){
+    std::string existingFavMovies;
+    std::string userDetailedLine;
+    const std::string filename = "user_details_cpp_dataset.csv";
+    std::string titleCopy = title;
+
+    // opening the file stream for userDatabase
+    std::ifstream userDatabase(filename);
+    std::string* userDetailsArr = new std::string[4];
+
+    // reading the file line by line and extracting the username which matches the arg
+    for(std::string line; getline(userDatabase, line); ){
+        // using find() function to check if the username is present in the row of the csv
+        if(line.find(username) != std::string::npos){
+            std::stringstream userDetailsInParts(line);
+            std::string separatedLine;
+            int i = 0; // iterator for user details
+            while(std::getline(userDetailsInParts, separatedLine, ',')){
+                userDetailsArr[i] = separatedLine;
+                i++;
+            }
+            break;
+        }
+    }
+    userDatabase.close();
+
+    userDetailedLine = userDetailsArr[3];
+    std::stringstream userDetailedLineInParts(userDetailedLine);
+    std::string separatedLine;
+    int totalNumOfMovies = 0;
+    int count = 0; // num of movies not matched with the parameter
+
+    // checking if the title of the movie is in user's fav movie list
+    while(std::getline(userDetailedLineInParts, separatedLine, ';')){
+        std::string tempMovieName = separatedLine;
+        totalNumOfMovies++;
+        // changing the title of the movie to lowercase
+        for(int i = 0; i < tempMovieName.length(); i++){
+            tempMovieName[i] = tolower(tempMovieName[i]);
+        }
+        // changing the title parameter to lowercase
+        for(int i = 0; i < title.length(); i++){
+            title[i] = tolower(title[i]);
+        }
+    
+        if(tempMovieName != title){
+            count++;
+        }
+    }
+
+    if(count == totalNumOfMovies){
+        std::cout << "Movie does not exist in favourites!" << std::endl;
+        return;
+    }
+
+
+    std::ifstream userDatabaseIn;
+    userDatabaseIn.open(filename);
+    int len = 0;
+    // getting the length of the total file
+    std::ifstream movieDatabaseTemp(filename);
+    std::string tempLine;
+    while(getline(movieDatabaseTemp, tempLine)){}
+    // finding the first word from the tempLine
+    std::string userIdInStr;
+    int commaIndexInUser = 0;
+    for(int i = 0; i < tempLine.length(); i++){
+        if(tempLine[i] == ','){
+            commaIndexInUser = i;
+            break;
+        }
+    }
+    for(int i = 0; i < commaIndexInUser; i++){
+        userIdInStr = userIdInStr + tempLine[i];
+    }
+    len = stoi(userIdInStr);
+    len += 2;
+    movieDatabaseTemp.close();
+
+    std::string *tempArr = new std::string[len];
+    int i = 0; 
+    for(std::string line; getline(userDatabaseIn, line); ){
+        if(i < len){
+            tempArr[i] = line;
+            i++;
+        }
+    }
+
+    std::string userDetailsInStr = userDetailsArr[0];
+    for(int i = 1; i < 3; i++){
+        userDetailsInStr = userDetailsInStr + "," + userDetailsArr[i];
+    }
+
+    std::stringstream tempUserDetailedLineInParts(userDetailsArr[3]);
+    std::string tempSeparatedLine;
+    std::string titleTemp = title;
+    std::string newFavMovieList = "";
+    // removing the title of the movie from user's fav list
+    while(std::getline(tempUserDetailedLineInParts, tempSeparatedLine, ';')){
+        std::string tempMovieName = tempSeparatedLine;
+
+        // changing the title of the movie to lowercase
+        for(int i = 0; i < tempMovieName.length(); i++){
+            tempMovieName[i] = tolower(tempMovieName[i]);
+        }
+        // changing the title parameter to lowercase
+        for(int i = 0; i < titleTemp.length(); i++){
+            titleTemp[i] = tolower(titleTemp[i]);
+        }
+    
+        if(tempMovieName != titleTemp){
+            newFavMovieList += tempMovieName + ";";
+        }
+    }
+    newFavMovieList.pop_back();// removing ; from the end of the line
+    std::string newUserDetailsInStr = userDetailsInStr + "," + newFavMovieList;
+    userDetailsInStr = userDetailsInStr +  "," + userDetailsArr[3];
+
+    for(int i = 0; i < len; i++){
+        if(tempArr[i] == userDetailsInStr){
+            userDetailsInStr = newUserDetailsInStr;
+            tempArr[i] = userDetailsInStr;
+        }
+    }
+    delete[] userDetailsArr;
+
+    int lenOfFilename = filename.length();
+    char* TempFilename = new char[lenOfFilename];
+    for(int i = 0; i < lenOfFilename; i++){
+        TempFilename[i] = filename[i];
+    }
+    
+    char newTempFilename[lenOfFilename];
+    for(int i = 0; i <lenOfFilename; i++){
+        newTempFilename[i] = TempFilename[i];
+    }
+    delete[] TempFilename;
+
+    int status = remove(newTempFilename);
+
+    std::ofstream userDatabaseOut;
+    userDatabaseOut.open(filename, std::ios::app);
+    userDatabaseOut << tempArr[0];
+    for(int i = 1; i < len; i++){
+        userDatabaseOut << "\n" << tempArr[i]; 
+    }
+    delete[] tempArr;
+    userDatabaseOut.close();
+}
--- User_preferences_main.cpp
+++ User_preferences_main.cpp
@@ -5,22 +5,12 @@
 #include "Preferences.h"
 
 int main(){
-    User_preferences user1;
-    std::cout << "\t\t\t\t\t\t\t | WELCOME TO MOVIE RECOMMENDOR | " << std::endl;
-    std::cout << std::endl;
-    std::cout << "This system helps you manage your favourite movies in one place while simultaneously helping you decide which movie to watch. " << std::endl;
-    std::cout << std::endl;
-    std::cout << "Do you wish to login or sign up?" << std::endl;
-    std::cout << "Enter 'login' login to login and 'signup' to signup: login" << std::endl;
-    user1.checkCredentials();
-    std::cout << std::endl;
-    user1.printFavMovies();
-    std::cout << std::endl;
-    std::cout << "Would you like to compare two movies or check if the a movie is worth your time? " << std::endl;
-    std::cout << "Enter 'compare' for comparing and 'worthy' for the latter: " << std::endl;
-    std::cout << "Enter 'other' to see more options: ";
-    std::string input;
-    std::cin>>input;
+     User_preferences user1;
+     user1.checkCredentials();
+     std::string title = "dhoom 2";
+     user1.removeMovieFromFavourites(title);
 
-    return 0;
+     user1.printFavMovies();
+
+     return 0;
 }

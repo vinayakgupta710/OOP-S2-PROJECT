847311403fbe2f234008d3481c88ca6ef57d9160
Vinayak Gupta <guptavinayak710@gmail.com>
2022-10-10 22:17:42 +1030

    added new header fileA

--- Preferences.h
+++ Preferences.h
@@ -1,7 +1,7 @@
 #ifndef PREFERENCES_H
 #define PREFERENCES_H
 
-class Prefernces: public User_preferences{
+class Preferences: public User_preferences{
 public:
     virtual void updateUserPreference() = 0;
     virtual void calculatePreferenceScore() = 0;
---
+++ Release_year_preferences.h
@@ -0,0 +1,18 @@
+#ifndef RELEASE_YEAR_PREFERENCES
+#define RELEASE_YEAR_PREFERENCES
+
+#include "Preferences.h"
+#include "User_preferences.h"
+#include <vector>
+
+class Release_year_preferences: public Preferences{
+private:
+    std::vector<int> releaseYear; // vector of release years of the fav movies of the user
+    float avgReleaseYear; // average release year of the movies
+public:
+    Release_year_preferences(); // default constructor
+    void updateUserPreference(); // adds the release year vector and calculates the average
+    void calculatePreferenceScore(); // calculates the preference score usign the algo
+};
+
+#endif

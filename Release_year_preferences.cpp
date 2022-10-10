57498d2fbf542a85275e2653acc7b1804d0ae9ca
Vinayak Gupta <guptavinayak710@gmail.com>
2022-10-10 23:17:12 +1030

    completed update user pref func

--- Release_year_preferences.cpp
+++ Release_year_preferences.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
 #include "User_preferences.h"
+#include "User_details.h"
 #include "Movies_database.h"
 #include "Preferences.h"
 #include "Release_year_preferences.h"
@@ -10,5 +14,30 @@ Release_year_preferences::Release_year_preferences(){
 }
 
 void Release_year_preferences::updateUserPreference(){
-    
+    Movies_database** favMovieList = new Movies_database*[favMovies.size()];
+    for(int i = 0; i < favMovies.size(); i++){
+        favMovieList[i] = new Movies_database;
+    }
+
+    for(int i = 0; i < favMovies.size(); i++){
+        Movies_database tempMovie;
+        std::string title = favMovies.at(i);
+        favMovieList[i] = tempMovie.fetchMovie(title);
+    }
+
+    for(int i = 0; i < favMovies.size(); i++){
+        int tempYear = favMovieList[i]->getReleaseYear();
+        releaseYear.push_back(tempYear);
+    }
+
+    delete[] favMovieList;
+    float sum = 0; 
+    for(int i = 0; i < favMovies.size(); i++){
+        sum += releaseYear.at(i);
+    }
+    avgReleaseYear = sum / favMovies.size();
+}
+
+void Release_year_preferences::calculatePreferenceScore(){
+
 }
---
+++ Release_year_preferences_main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include "User_preferences.h"
+#include "User_details.h"
+#include "Movies_database.h"
+#include "Preferences.h"
+#include "Release_year_preferences.h"
+
+int main(){
+    Release_year_preferences user1;
+    user1.checkCredentials();
+    user1.printFavMovies();
+
+    user1.updateUserPreference();
+
+    return 0;
+}

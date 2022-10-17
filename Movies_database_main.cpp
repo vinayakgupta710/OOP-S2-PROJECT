60b3f76c7d68c9ab61ce4684a2a39ad0bcbe1651
Vinayak Gupta <guptavinayak710@gmail.com>
2022-10-17 14:38:59 +1030

    unit test v1 done

--- Movies_database_main.cpp
+++ Movies_database_main.cpp
@@ -6,11 +6,24 @@
 int main(){
     Movies_database* movie = new Movies_database;
     std::string title;
-    std::cout << "enter: ";
+    std::cout << "Enter a movie name: ";
     getline(std::cin, title);
     movie = movie->fetchMovie(title);
     std::string name = movie->getTitle();
-    std::cout << "Inside main: " << name << std::endl;
+
+    std::cout << std::endl << "Title of the movie: " << movie->getTitle() << std::endl;
+    std::cout << "Release year of the movie: " << movie->getReleaseYear() << std::endl;
+    std::cout << "Rating of the movie (as per TMDb): " << movie->getRating() << std::endl;
+    std::cout << "Production stduio of the movie: " << movie->getStudio() << std::endl;
+    std::cout << "Genres of the movie: ";
+    std::string *genres = movie->getGenreList();
+    for(int i = 0; i < movie->getNumOfGenres(); i++){
+        if(i < movie->getNumOfGenres() - 1){
+            std::cout << *(genres + i) << ", ";
+        } else {
+            std::cout << *(genres + i) << std::endl;
+        }
+    }
 
     return 0;
 }
